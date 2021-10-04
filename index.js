183c3dcd918879c15bc7b00badcf03911d07af7c
David Luedtke <david@ldtke.de>
2021-10-04 04:15:50 +0200

    error handling

--- index.js
+++ index.js
@@ -8,38 +8,58 @@ function sleep(ms) {
     });
 }
 
-async function vote(minecraftName, serverId) {    
+async function vote(minecraftName, serverId) {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.goto('https://minecraft-server.eu/vote/index/' + serverId + '/' + minecraftName);
+    try {
+        const page = await browser.newPage();
+        await page.goto('https://minecraft-server.eu/vote/index/' + serverId + '/' + minecraftName);
 
-    console.log('Started voting, waiting for drunken cow…');
+        console.log('Started voting, waiting for drunken cow…');
 
-    await page.waitForSelector('#playername', {visible: true});
-    await sleep(Math.floor(Math.random() * 1500) + 500);
+        await page.waitForSelector('#playername', {visible: true});
+        await sleep(Math.floor(Math.random() * 1500) + 500);
 
-    await page.waitForSelector('#captcha', {visible: true});
-    page.click('#captcha');
+        await page.waitForSelector('#captcha', {visible: true});
+        page.click('#captcha');
 
-    console.log('Voted, waiting for response…');
+        console.log('Voted, waiting for response…');
 
-    await page.waitForNavigation();
-    await page.waitForSelector('.card .alert');
-    const alert = await page.$(".card .alert");
-    const text = await page.evaluate(alert => alert.textContent, alert);
+        await page.waitForNavigation();
+        await page.waitForSelector('.card .alert');
+        const alert = await page.$(".card .alert");
+        const text = await page.evaluate(alert => alert.textContent, alert);
 
-    console.log(text);
-    await browser.close();
+        console.log(text);
+    } catch(e) {
+        return e;
+    } finally {
+        await browser.close();
+    }
 
     return text;
 }
 
 async function sendDiscordMessage(webhook, message, minecraftName) {
-    const embed = new MessageBuilder()
-        .setTitle('Voted for: ' + minecraftName)
-        .setAuthor('Vote-Bot', 'https://dl.kinau.systems/loading.gif')
-        .setColor('#00b0f4')
-        .setDescription(message);
+    var embed;
+    if (message instanceof String) {
+        embed = new MessageBuilder()
+            .setTitle('Voted for: ' + minecraftName)
+            .setAuthor('Vote-Bot', 'https://dl.kinau.systems/loading.gif')
+            .setColor('#00b0f4')
+            .setDescription(message);
+    } else if (message instanceof Error) {
+        embed = new MessageBuilder()
+            .setTitle('Could not vote for: ' + minecraftName)
+            .setAuthor('Vote-Bot', 'https://dl.kinau.systems/loading.gif')
+            .setColor('#ffbaba')
+            .setDescription(message.message);
+    } else {
+        embed = new MessageBuilder()
+            .setTitle('Could not vote for: ' + minecraftName)
+            .setAuthor('Vote-Bot', 'https://dl.kinau.systems/loading.gif')
+            .setColor('#ffbaba')
+            .setDescription("unknown error:\n " + message);
+    }
      
     webhook.send(embed);
 }
